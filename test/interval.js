cc2cb428de29a98257b27aebca506592e35f0c04
Trygve Lie <github@trygve-lie.com>
2014-10-30 19:35:37 +0100

    Initial code commit

---
+++ test/interval.js
@@ -0,0 +1,25 @@
+/* jshint node: true, strict: true */
+/* global describe: true, it: true, before: true */
+
+"use strict";
+
+var mocha       = require('mocha'),
+    assert      = require('chai').assert,
+    Interval    = require('../');
+
+
+
+
+describe('a()', function(){
+
+    describe('b', function(){
+
+        it('c', function(){
+
+            assert.equal('a', 'a');
+
+        });
+
+    });
+
+});
